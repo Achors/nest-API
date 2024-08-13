7f2f1807e983ef21895cb6b83d9a6a74ef222403
Adolf <adolfnyerere@gmail.com>
2024-08-14 02:56:11 +0300

    database connection

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,4 +1,6 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AirtimeController } from './airtime/airtime.controller';
@@ -6,10 +8,28 @@ import { AirtimeService } from './airtime/airtime.service';
 import { AirtimeModule } from './airtime/airtime.module';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
+import { JwtStrategy } from './auth/jwt.strategy';
 
 @Module({
-  imports: [AirtimeModule, UsersModule, AuthModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        url: configService.get<string>('DATABASE_UR'),
+        entities: [__dirname + '/../**/*.entity{.ts,.js}'],
+        synchronize: true,
+      }),
+    }),
+    AirtimeModule,
+    UsersModule,
+    AuthModule,
+  ],
   controllers: [AppController, AirtimeController],
-  providers: [AppService, AirtimeService],
+  providers: [JwtStrategy, AppService, AirtimeService],
 })
 export class AppModule {}
